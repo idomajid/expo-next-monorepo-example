f7e03da4096ab597626ac3d8bbc10caade5382f6
IDO Majid <idomajid99@gmail.com>
2021-11-11 20:32:23 +0700

    Create userData and dummy Data

---
+++ packages/expo/constants/userData.js
@@ -0,0 +1,12 @@
+class UserData {
+  constructor(userId, name, nationality, date, imageUrl, opinion) {
+    this.userId = userId
+    this.name = name
+    this.nationality = nationality
+    this.date = date
+    this.imageUrl = imageUrl
+    this.opinion = opinion
+  }
+}
+
+export default UserData
---
+++ packages/expo/data/dummy-data.js
@@ -0,0 +1,12 @@
+import userData from '../constants/userData'
+
+export const User = [
+  new userData(
+    'm10',
+    'John Biles',
+    'American',
+    '01-30-2021',
+    'https://cdn.pixabay.com/photo/2018/04/09/18/26/asparagus-3304997_1280.jpg',
+    'FUck Trump'
+  )
+]
