5933d4e0eb850cbc649ed52b08eac047a84e2c37
IDO Majid <idomajid99@gmail.com>
2021-11-29 16:33:52 +0700

    replace flatlist fetch from dummy-data to firebase

--- packages/app/screens/home.tsx
+++ packages/app/screens/home.tsx
@@ -1,31 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FlatList, StyleSheet } from 'react-native'
 import { View, Text, TextInput } from 'dripsy'
 import { USER } from '../../expo/data/dummy-data'
+import { ref, child, get } from 'firebase/database'
 
 //import { getDatabase, ref, set, get } from "firebase/database";
 
 import Card from '../components/Card'
+import { db } from '../../expo/firebase'
 
 export default function HomeScreen(props) {
+  const [articleList, setArticleList] = useState([])
 
-  //const gdb = getDatabase();
+  useEffect(() => {
+    const dbRef = ref(db)
+    let articlesList = []
+    get(child(dbRef, `items/`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          snapshot.forEach((snap) => {
+            articlesList.push(snap.val())
+          })
+
+          setArticleList(articlesList)
+        } else {
+          console.log('No data available')
+        }
+      })
+      .catch((error) => error.message)
+  }, [])
 
   return (
     <View style={styles.container}>
-      
       <View style={{ flex: 1 }}>
         <FlatList
-          data={USER}
+          data={articleList}
           renderItem={({ item }) => (
             <Card
               name={item.name}
               date={item.date}
-              opinion={item.opinion}
-              image={item.imageUrl}
+              opinion={item.quote}
+              image={item.avatarUrl}
             />
           )}
-          keyExtractor={(item) => item.userId}
+          keyExtractor={(item) => item.uuid}
         />
       </View>
     </View>
