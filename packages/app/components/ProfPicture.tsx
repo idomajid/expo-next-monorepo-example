d5b2378e40b8cdb8b5339eecb08bd0be05dfec35
IDO Majid <idomajid99@gmail.com>
2021-12-11 11:59:40 +0700

    deleted

--- packages/app/components/ProfPicture.tsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import { View, Image } from 'react-native'
-
-const ProfPicture = (props) => (
-  <Image style={props.style} source={props.source} />
-)
-export default ProfPicture
