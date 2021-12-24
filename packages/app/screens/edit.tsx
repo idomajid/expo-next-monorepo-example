094e57cb0ae1e8d2f0ca4385bc8fbf8b6faea94f
IDO Majid <idomajid99@gmail.com>
2021-12-24 09:27:21 +0700

    adding edit feature

--- packages/app/screens/edit.tsx
+++ packages/app/screens/edit.tsx
@@ -1,19 +1,84 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet } from 'react-native'
 import Form from '../components/FormPost'
+import type { HomeScreenProps } from 'app/navigation/types'
 
-export default function edit() {
-    return (
-        <View style={styles.container}>
-            <Form/>
-        </View>
-    )
+import { ref, update, child, get } from 'firebase/database'
+
+import { db, auth } from '../../expo/firebase'
+import { useRouter } from 'app/navigation/use-router'
+
+export default function edit({ navigation, route }: HomeScreenProps) {
+  const [mytitle, setMyTitle] = useState('')
+  const [myArticle, setMyArticle] = useState('')
+  const [myAvatar, setMyAvatar] = useState('')
+  const [picture, setPicture] = useState('')
+
+  const uid = route.params.uid
+  const router = useRouter()
+  const user = auth.currentUser
+
+  
+
+  useEffect(() => {
+    const dbRef = ref(db)
+    get(child(dbRef, `items/${uid}`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const value = snapshot.val()
+
+          setMyTitle(value.title)
+          setMyArticle(value.quote)
+          setMyAvatar(value.avatarUrl)
+
+        } else {
+          console.log('No data available')
+        }
+      })
+      .catch((error) => error.message)
+  }, [])
+
+  const updateData = () => {
+    update(ref(db, `items/${uid}`), {
+      title: mytitle,
+      avatarUrl: myAvatar,
+      quote: myArticle
+    })
+      .then(() => {
+        router.push('/home')
+      })
+      .catch((error) => error.message)
+  }
+
+  if (!user) {
+    router.push('/profile')
+  }
+
+  // if (getDataForm == null) return <Text>Loading</Text>
+
+  return (
+    <View style={styles.container}>
+      <Form
+        titleValue={mytitle}
+        //defaultTitleValue={getDataForm.title}
+        titleOnChange={(text) => setMyTitle(text)}
+        avatarValue={myAvatar}
+        //defaultAvatarValue={getDataForm.avatarUrl}
+        avatarOnChage={(text) => setMyAvatar(text)}
+        articleValue={myArticle}
+        //defaultArticleValue={getDataForm.quote}
+        articleOnChange={(text) => setMyArticle(text)}
+        onInsertData={updateData}
+        titleButton="Update Post"
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      width: '100%',
-      height: '100%',
-      backgroundColor: '#428ACA'
-    }
-  })
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#428ACA'
+  }
+})
--- packages/app/screens/home.tsx
+++ packages/app/screens/home.tsx
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { FlatList, StyleSheet, Pressable, Button } from 'react-native'
-import { View, Text, TextInput } from 'dripsy'
-import { USER } from '../../expo/data/dummy-data'
-import { ref, child, get } from 'firebase/database'
+import { FlatList, StyleSheet, Pressable} from 'react-native'
+import { View } from 'dripsy'
+
+
+import { ref, child, get} from 'firebase/database'
 import { useRouter } from 'app/navigation/use-router'
 
-
-//import { getDatabase, ref, set, get } from "firebase/database";
 
 import Card from '../components/Card'
 import { db } from '../../expo/firebase'
 
-export default function 
-
-HomeScreen(props) {
+export default function HomeScreen() {
   const [articleList, setArticleList] = useState([])
 
   const router = useRouter()
@@ -24,6 +21,7 @@ HomeScreen(props) {
     get(child(dbRef, `items/`))
       .then((snapshot) => {
         if (snapshot.exists()) {
+
           snapshot.forEach((snap) => {
             articlesList.push(snap.val())
           })
@@ -32,9 +30,21 @@ HomeScreen(props) {
         } else {
           console.log('No data available')
         }
+      
+        
       })
       .catch((error) => error.message)
   }, [])
+
+  // const RemoveData = () => {
+  //   const uuid = myId.uuid
+  //   console.log(uuid)
+  //   remove(ref(db, `items/${uuid}`))
+  //     .then(() => {
+  //       Alert.alert('deleted')
+  //     })
+  //     .catch((error) => error.message)
+  // }
 
   return (
     <View style={styles.container}>
@@ -46,7 +56,6 @@ HomeScreen(props) {
             key={item.uuid}
           onPress={() => {
             router.push(`/article/${item.uuid}`)
-            //console.log(item.uuid)
           }}
             >
             <Card
@@ -64,8 +73,6 @@ HomeScreen(props) {
           keyExtractor={(item) => item.uuid}
         />
       </View>
-      
-      
     </View>
   )
 }
