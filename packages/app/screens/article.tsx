7591ef68622fc63ad9ec586503efe8d7d07cc65d
IDO Majid <idomajid99@gmail.com>
2021-12-28 19:21:34 +0700

    condition delete and edit who doesnt have access

--- packages/app/screens/article.tsx
+++ packages/app/screens/article.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'dripsy'
 import { StyleSheet, Alert, ActivityIndicator } from 'react-native'
 
 import { ref, child, get, remove } from 'firebase/database'
-import { db } from '../../expo/firebase'
+import { db, auth } from '../../expo/firebase'
 
 import { useRouter } from 'app/navigation/use-router'
 import type { HomeScreenProps } from 'app/navigation/types'
@@ -24,57 +24,74 @@ export default function ArticleScreen({ navigation, route }: HomeScreenProps) {
   const routeParams = route.params.id
   const router = useRouter()
 
+  const user = auth.currentUser
+
   useEffect(() => {
     const dbRef = ref(db)
-  get(child(dbRef, `items/${routeParams}`))
+    get(child(dbRef, `items/${routeParams}`))
       .then((snapshot) => {
         if (snapshot.exists()) {
-          
           setArticle(snapshot.val())
         } else {
-          console.log("No data available");
+          console.log('No data available')
         }
       })
       .catch((error) => error.message)
   }, [])
 
-  
+  if (article == undefined) {
+    return <ActivityIndicator />
+  }
 
   const RemoveArt = () => {
-    remove(ref(db, `items/${routeParams}`))
-      .then(() => {
-        Alert.alert('deleted')
-        router.push(`/home`)
-      })
-      .catch((error) => error.message)
+    if (article.userId != user.uid) {
+      Alert.alert('Article is not belong to you')
+      router.push(`/home`)
+    } else {
+      remove(ref(db, `items/${routeParams}`))
+        .then(() => {
+          router.push(`/home`)
+        })
+        .catch((error) => error.message)
+    }
   }
 
   const RemoveAlert = () => {
-    Alert.alert(
-      "Are You Sure ",
-      " Delete the article ?",
-      [
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel"
-        },
-        { text: "OK", onPress: RemoveArt }
-      ]
-    );
-  } 
+    Alert.alert('Are You Sure ', ' Delete the article ?', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel'
+      },
+      { text: 'OK', onPress: RemoveArt }
+    ])
+  }
 
-  if (article == null) return <ActivityIndicator style={{flex: 1, justifyContent: "center", alignItems: "center"}}/>
-
+  if (article == null)
+    return (
+      <ActivityIndicator
+        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+      />
+    )
 
   const routerUuid = () => {
-    router.push(`/${article.uuid}/edit`)
+    if (article.userId != user.uid) {
+      Alert.alert('You dont have a permission to access this page')
+      router.push(`/home`)
+    } else {
+      router.push(`/${article.uuid}/edit`)
+    }
   }
 
   return (
     <View style={styles.container}>
       <View>
-      <Article title={article.title} paragraph={article.quote} deleteArticleHandle={RemoveAlert} editHandle={routerUuid}/>
+        <Article
+          title={article.title}
+          paragraph={article.quote}
+          deleteArticleHandle={RemoveAlert}
+          editHandle={routerUuid}
+        />
       </View>
     </View>
   )
